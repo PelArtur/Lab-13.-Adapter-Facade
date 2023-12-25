bceb6f9d82295adac6d360abe7a20447f8e65aa4
Artur Pelcharskyi <pelcharskyi.pn@ucu.edu.ua>
2023-12-25 23:40:36 +0200

    initial commit

---
+++ lab13/src/test/java/com/example/lab13/Lab13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
